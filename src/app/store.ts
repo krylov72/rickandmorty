be4ac1f76d034861f62ec1fa568d429d25a4e28a
krylov72 <qobject1337@gmail.com>
2024-10-02 14:41:51 +0500

    configure store

---
+++ src/app/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { cardsReducer } from '../pages/cards/cards-reducer';
+
+export const store = configureStore({
+  reducer: cardsReducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
