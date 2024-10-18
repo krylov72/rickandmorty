75c25a988d50e4c1527fb90e2b7e416ddc409c9c
krylov72 <qobject1337@gmail.com>
2024-10-18 09:24:47 +0500

    add router

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import App from '../src/app/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
 
