96da4ac724d988837bde50f4d3f056c6ed36fa08
krylov72 <qobject1337@gmail.com>
2024-10-18 09:26:10 +0500

    add query parametrs and navigation

--- src/pages/cards/Cards.tsx
+++ src/pages/cards/Cards.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import logo from '../assets/pngwing.com.png';
 import Card from './Card';
 import { useEffect, useState } from 'react';
 import { useAppDispatch } from '../../common/useAppDispatch';
@@ -7,35 +6,54 @@ import { fetchCards } from './cards-reducer';
 import { useSelector } from 'react-redux';
 import { cardsSelector, isLoadingSelector } from './cardsSelector';
 import { Info } from '../../api/cardsApi';
-import { RootState } from '../../app/store';
 import { Loader } from '../../common/Loader';
+import { ParticlesSX } from '../../common/Particles';
+import { Filter } from '../filter/Filter';
+import { filterSelect } from '../filter/filterSelect';
+import { filterActions } from '../filter/filter-slice';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Cards = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
   const isLoading = useSelector(isLoadingSelector);
-  const [currentApiUrl, setCurrentApiUrl] = useState<string>('https://rickandmortyapi.com/api/character/?page=1');
+  const filter = useSelector(filterSelect);
+  const cards = useSelector(cardsSelector);
+
+  const currentApiUrl = `https://rickandmortyapi.com/api/character${location.search}`;
+
   const [info, setInfo] = useState<Info | null>(null);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCards(currentApiUrl)).then((res) => {
+      console.log(res);
+
+      if (typeof res.payload !== 'string' && res.payload) {
+        setInfo(res.payload.info);
+      }
+    });
+  }, [currentApiUrl, dispatch]);
+
+  const changeFilterHandler = (name: string) => {
+    dispatch(filterActions.setFilterName({ name }));
+    const params = new URLSearchParams(location.search);
+    params.set('name', name); // Замените 'filter' на нужный вам параметр
+
+    navigate(`?${params.toString()}`);
+  };
 
   const nextPageHandler = () => {
     if (info?.next) {
-      setCurrentApiUrl(info?.next);
+      navigate(`${info?.next.slice(32)}`);
     }
   };
 
   const prevPageHandler = () => {
     if (info?.prev) {
-      setCurrentApiUrl(info?.prev);
+      navigate(`${info?.prev.slice(32)}`);
     }
   };
-  const dispatch = useAppDispatch();
-
-  const cards = useSelector(cardsSelector);
-  useEffect(() => {
-    dispatch(fetchCards(currentApiUrl)).then((res) => {
-      if (typeof res.payload !== 'string' && res.payload) {
-        setInfo(res.payload.info);
-      }
-    });
-  }, [currentApiUrl]);
 
   if (isLoading) {
     return <Loader />;
@@ -43,21 +61,26 @@ export const Cards = () => {
 
   return (
     <>
+      <ParticlesSX />
       <CardsContainer>
-        {cards.map((c) => {
-          return (
-            <Card
-              key={c.id}
-              status={c.status}
-              location={c.location.name}
-              name={c.name}
-              species={c.species}
-              gender={c.gender}
-              avatar={c.image}
-            ></Card>
-          );
-        })}
+        <CardsGrid>
+          {cards.map((c) => {
+            return (
+              <Card
+                key={c.id}
+                status={c.status}
+                location={c.location.name}
+                name={c.name}
+                species={c.species}
+                gender={c.gender}
+                avatar={c.image}
+              />
+            );
+          })}
+        </CardsGrid>
+        <Filter name={filter} onChangeFilter={changeFilterHandler} />
       </CardsContainer>
+
       <button onClick={prevPageHandler} disabled={!info?.prev}>
         PREV
       </button>
@@ -70,6 +93,15 @@ export const Cards = () => {
 
 const CardsContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, minmax(200px, 1fr));
-  gap: 10px;
+  grid-template-columns: 1fr minmax(200px, 400px);
+  gap: 20px;
+  height: auto;
+  min-height: 100vh;
+`;
+
+const CardsGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(200px, 900px)); /* Адаптивная сетка для карточек */
+  gap: 5px;
+  height: fit-content;
 `;
