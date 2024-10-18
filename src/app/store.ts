4912e87ee9049111252b779c573f86255319d89d
krylov72 <qobject1337@gmail.com>
2024-10-18 09:25:08 +0500

    add filter reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { cardsReducer } from '../pages/cards/cards-reducer';
+import { filterActions, filterReducer } from '../pages/filter/filter-slice';
 
 export const store = configureStore({
-  reducer: cardsReducer,
+  reducer: {
+    cards: cardsReducer,
+    filter: filterReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
